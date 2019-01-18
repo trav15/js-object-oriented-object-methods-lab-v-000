bce511647928ceda58a73c2d66b5ac99db657f81
Travis T <travistokuyama@gmail.com>
2019-01-18 03:26:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function BoardMember(name, homeState, training){
   }
   
   this.sayHi = function() {
-    return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in <training>.`
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
   }
   
 }
